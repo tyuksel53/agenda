9e2bed2b9a4708556052e1e0fe38a74caadffdde
Taha Yüksel <taha.yuksel053@gmail.com>
2022-08-05 18:37:23 +0300

    home screen design finished
    .

--- ViewController.swift
+++ ViewController.swift
@@ -10,14 +10,46 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var progress: KYCircularProgress!
+    @IBOutlet weak var inProgressChart: KYCircularProgress!
+    @IBOutlet weak var toDoChart: KYCircularProgress!
+    
+    @IBOutlet weak var totalSummaryTotalTask: UILabel!
+    
+    @IBOutlet weak var totalSummaryDone: UILabel!
+    @IBOutlet weak var totalSummaryToDo: UILabel!
+    @IBOutlet weak var totalSummaryInProgess: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         progress.colors = [UIColor(red: 0.94, green: 0.60, blue: 0.36, alpha: 1.00)]
-        progress.progress = 0.7
+        progress.progress = 0.2
         
-        //progress.transform = progress.transform.rotated(by: .pi * 1.5)
-        // Do any additional setup after loading the view.
+        inProgressChart.colors = [UIColor(red: 0.49, green: 0.85, blue: 0.51, alpha: 1.00)]
+        inProgressChart.progress = 0.15
+        
+        
+        toDoChart.colors = [UIColor(red: 0.46, green: 0.76, blue: 0.86, alpha: 1.00)]
+        toDoChart.progress = 0.65
+        
+        
+        totalSummaryTotalTask.attributedText = changeColorText(mainString: "Total Tasks: 5", stringToColor: "5")
+        totalSummaryDone.attributedText = changeColorText(mainString: "Done: 1", stringToColor: "1")
+        totalSummaryToDo.attributedText = changeColorText(mainString: "To Do: 1", stringToColor: "1")
+        totalSummaryInProgess.attributedText = changeColorText(mainString: "In Progress: 3", stringToColor: "3")
+     
+    }
+    
+    func changeColorText(mainString: String, stringToColor: String) ->  NSMutableAttributedString {
+        
+        let range = (mainString as NSString).range(of: stringToColor)
+
+        let mutableAttributedString = NSMutableAttributedString.init(string: mainString)
+        
+        let color = UIColor(red: 0.94, green: 0.60, blue: 0.36, alpha: 1.00)
+        
+        mutableAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: color, range: range)
+        
+        return mutableAttributedString
     }
 
 
