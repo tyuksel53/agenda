8339c0794db810ad3a26b9b41e8d21890551c916
Taha Yüksel <taha.yuksel053@gmail.com>
2022-08-05 16:22:27 +0300

    more design things
    home screen

--- ViewController.swift
+++ ViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progress: KYCircularProgress!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        progress.colors = [UIColor(red: 0.94, green: 0.60, blue: 0.36, alpha: 1.00)]
+        progress.progress = 0.7
+        
+        //progress.transform = progress.transform.rotated(by: .pi * 1.5)
         // Do any additional setup after loading the view.
     }
 
