0756b51641ca1c84b8473eea8560d110dbb32c32
Taha Yüksel <taha.yuksel053@gmail.com>
2022-09-01 19:17:25 +0300

    insert screen
    design stuff

---
+++ Controller/InsertTaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InsertTaskViewController.swift
+//  Agenda
+//
+//  Created by Taha Yüksel on 1.09.2022.
+//
+
+import UIKit
+
+class InsertTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
